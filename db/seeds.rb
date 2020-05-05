a23fae9841c8aa9fd9b47e9b68024fd3a65128c1
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2020-05-05 22:53:06 +0200

    add: seeds for various record options

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+article = Article.create(title: "Version 1", body: Faker::Lorem.paragraph)
+
+2.upto(6) do |i|
+  article.update(title: "Version #{i}")
+end
+
+1.upto(2) do |i|
+  deleted_article = Article.create(title: "Deleted Article #{i}", body: Faker::Lorem.paragraph)
+  deleted_article.destroy
+  deleted_article = Article.new(id: deleted_article.id).versions.last.reify
+  deleted_article.save
+  deleted_article.update(title: "Deleted Article #{i} Version 2")
+  deleted_article.destroy
+end
+
+restored_article = Article.create(title: 'A previously deleted article', body: Faker::Lorem.paragraph)
+restored_article.destroy
+restored_article = Article.new(id: restored_article.id).versions.last.reify
+restored_article.save
