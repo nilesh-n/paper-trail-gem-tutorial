47bbfdf0d011fb630bd0746fdc5cc91f08ab7ccc
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2020-05-22 16:10:39 +0200

    change: update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_122050) do
+ActiveRecord::Schema.define(version: 2020_05_22_140352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2020_05_22_122050) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "note_images", force: :cascade do |t|
+    t.bigint "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_note_images_on_note_id"
+  end
+
   create_table "notes", force: :cascade do |t|
     t.text "body"
     t.bigint "comment_id", null: false
@@ -83,5 +90,6 @@ ActiveRecord::Schema.define(version: 2020_05_22_122050) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "articles"
+  add_foreign_key "note_images", "notes"
   add_foreign_key "notes", "comments"
 end
