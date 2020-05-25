0b16d561dee7578ee6a48ae2aff88e8e4ee75eef
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2020-05-25 14:57:56 +0200

    feat: when deleting note, make note_id on note image nil
    Instead of deleting an note image, we make the note_id nil.
    This is so that we can restore it with paper trail.
    We also update each instead of update_all so that the callbacks
    for paper trail trigger correctly.

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,9 @@ class Note < ApplicationRecord
 
   belongs_to :comment
   has_many :note_images
+
+  before_destroy do
+    # note_images.update_all(note_id: nil)
+    note_images.each { |note_image| note_image.update(note_id: nil) }
+  end
 end
