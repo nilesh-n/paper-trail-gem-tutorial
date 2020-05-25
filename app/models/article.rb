5d576701d9e4c99b18f52e5e6058023dd8c0d559
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2020-05-25 12:18:08 +0200

    add: through association for note images on article
    for paper trail association tracking gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :notes, through: :comments
+  has_many :note_images, through: :notes
   has_one_attached :image
 end
